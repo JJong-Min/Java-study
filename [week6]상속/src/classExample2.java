08dd6e1d489ddd759b4becc8012b50fd2e0597b4
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-06-18 20:26:17 +0900

    클래스 상속실습
    클래스 상속실습을 위한 자식 클래스 코드

---
+++ [week6]상속/src/classExample2.java
@@ -0,0 +1,19 @@
+
+public class classExample2 extends classExample1 {
+	
+	int field2 = 5;
+	
+	classExample2() {
+		
+	}
+	
+	void method2 () {
+		System.out.println("class2의 method입니다.");
+	}
+
+	public static void main(String[] args) {
+		classExample2 a = new classExample2();
+
+	}
+
+}
