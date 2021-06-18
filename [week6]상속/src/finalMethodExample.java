3ded6029b5658c4faa3ba3c72609165443885200
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-06-18 20:28:36 +0900

    final method 실습
    final method 실습을 위한 자식 클래스 코드

---
+++ [week6]상속/src/finalMethodExample.java
@@ -0,0 +1,10 @@
+
+public class finalMethodExample extends finalMethod {
+	
+	public void method() {
+		// Cannot override the final method from finalMethod
+		// 위의 문구와 함께 경고창 발생.
+		// final로 선언된 메소드는 오버라이딩할 수 없음.
+	}
+
+}
