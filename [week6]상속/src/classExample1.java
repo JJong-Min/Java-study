20dd6fe30935ceaf223f06b9c89ddb4f7a5b4d68
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-06-18 20:26:01 +0900

    클래스상속 실습
    클래스 상속실습을 위한 부모 클래스 코드

---
+++ [week6]상속/src/classExample1.java
@@ -0,0 +1,20 @@
+
+public class classExample1 {
+	
+	int field1 = 10;
+	
+	classExample1() {
+		
+	}
+	
+	void method1 () {
+		System.out.println("class1의 method1 입니다.");
+	}
+
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
