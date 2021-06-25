c84185adf7e5bdaf69b89db0478753ee734fcdd6
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-06-25 21:20:12 +0900

    익명 구현 객체 실습
    익명 구현 객체 실습을 위한 코드

---
+++ [week8]인터페이스/src/RemoteControlExample.java
@@ -0,0 +1,13 @@
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl r = new RemoteControl() {
+			public void turnOn() {/*실행문*/}
+			public void turnOff() {/*실행문*/}
+			public void setVolume(int volume) {/*실행문*/}
+		};
+
+	}
+
+}
