411ebdd8845882754b3ad2fb05a85dbe4bf8e655
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-07-14 18:55:36 +0900

    멀티스레드 공유객체 실습
    멀티스레드 공유객체 실습을 위한 스레드2 코드입니다.

---
+++ [week10]Multi Thread/src/User2.java
@@ -0,0 +1,15 @@
+package Thread;
+
+public class User2 extends Thread {
+	private Calculator calculator;
+	
+	public void setCalculator(Calculator calculator) {
+		this.setName("CalculatorUser2");
+		this.calculator = calculator;
+	}
+	
+	public void run() {
+		calculator.setMemory(50);
+	}
+
+}
