c5099d7c7cfdd3473378f057ead47a334b35fdf3
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-07-18 23:26:05 +0900

    Generic 실습2
    제네릭 없이 만든 클래스 코드입니다.

---
+++ [week14]Generic/NoneGenericBox.java
@@ -0,0 +1,9 @@
+public class NoneGenericBox {
+    private Object object;
+    public void set(Object object) {
+        this.object = object;
+    }
+    public Object get() {
+        return object;
+    }
+}
