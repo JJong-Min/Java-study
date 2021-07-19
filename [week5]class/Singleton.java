2d878675c711dbd90f5d02d5e61ac7db9c3bccc8
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-07-20 08:14:42 +0900

    Singletone 실습
    sigletone을 알아보기 위해 생성한 클래스 코드입니다.

---
+++ [week5]class/Singleton.java
@@ -0,0 +1,14 @@
+package javastatic;
+
+public class Singleton {
+    private static Singleton one;
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if(one==null) {
+            one = new Singleton();
+        }
+        return one;
+    }
+}
