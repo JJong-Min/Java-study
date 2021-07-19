6a26660fc27b88b5d597a6ed28f725c7a171d401
JJong-Min <79436533+JJong-Min@users.noreply.github.com>
2021-07-20 08:15:21 +0900

    Singletone 실습
    singletone으로 생성되어지는지 알아보기 위한 실습 코드입니다.

---
+++ [week5]class/SingletoneTest.java
@@ -0,0 +1,12 @@
+package javastatic;
+
+public class SingletoneTest {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);
+    }
+
+
+
+}
