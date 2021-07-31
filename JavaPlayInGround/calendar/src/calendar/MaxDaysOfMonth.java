484c023795ab71ecc42acf5ab6586202d017ecd7
JJong-Min <sambujakkk@naver.com>
2021-07-31 21:39:09 +0900

    Add 반복횟수를 받아 반복 출력
    반복횟수를 입력받아 해당 수만큼 반복하여 사용자에게 입력받은 달의 최대 일수를 출력하는 코드입니다.

--- JavaPlayInGround/calendar/src/calendar/MaxDaysOfMonth.java
+++ JavaPlayInGround/calendar/src/calendar/MaxDaysOfMonth.java
@@ -11,17 +11,18 @@ public class MaxDaysOfMonth {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("달을 입력하세요.");
-        int month;
-        month = scanner.nextInt();
-//
-//        int [] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31};
-//        System.out.printf("%d월의 최대 일수는 %d입니다.", month, maxDays[month - 1]);
-//        scanner.close();
+        System.out.println("반복할 수를 입력하세요.");
+        int cnt;
+        cnt = scanner.nextInt();
+        for(int i = 0; i < cnt; i++) {
+            System.out.println("달을 입력하세요.");
+            int month;
+            month = scanner.nextInt();
 
-        MaxDaysOfMonth maxDaysOfMonth = new MaxDaysOfMonth();
-        int maxDay = maxDaysOfMonth.getMaxDayOfMonth(month);
-        System.out.printf("%d월의 최대 일수는 %d입니다.", month, maxDay);
+            MaxDaysOfMonth maxDaysOfMonth = new MaxDaysOfMonth();
+            int maxDay = maxDaysOfMonth.getMaxDayOfMonth(month);
+            System.out.printf("%d월의 최대 일수는 %d입니다.", month, maxDay);
+        }
 
     }
 }
