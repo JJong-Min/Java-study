777332baf5abe5e2d83ba4cec223bde7fa7ea4f4
JJong-Min <sambujakkk@naver.com>
2021-07-31 22:41:00 +0900

    가상 달력 출력 코드
    실제 달력이 아닌 모든 달의 1일이 일요일부터 시작하는 달력을 출력하는 클래스 코드입니다.

---
+++ JavaPlayInGround/calendar/src/calendar/Calendar.java
@@ -0,0 +1,28 @@
+package calendar;
+
+public class Calendar {
+
+    public static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31};
+
+    public int getMaxDayOfMonth(int month) {
+        return MAX_DAYS[month - 1];
+    }
+
+    public void printCalendar(int year, int month) {
+        System.out.printf("    <<%4d%3d>>\n", year, month);
+        System.out.println(" SU MO TU WE TH FR SA");
+        System.out.println("=--------------------");
+
+        int maxDay = getMaxDayOfMonth(month);
+
+        for (int i = 1; i <= maxDay; i++) {
+            System.out.printf("%3d", i);
+            if (i%7 == 0) {
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+
+    }
+}
