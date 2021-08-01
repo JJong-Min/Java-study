7fd005146d122a975a7d9b7e89721661aa56a54b
JJong-Min <sambujakkk@naver.com>
2021-08-01 20:25:06 +0900

    Update Prompt class
    요일을 받는 기능을 삭제하였습니다.

--- JavaPlayInGround/calendar/src/calendar/Prompt.java
+++ JavaPlayInGround/calendar/src/calendar/Prompt.java
@@ -6,10 +6,6 @@ import java.util.Arrays;
 public class Prompt {
 
     private final static String PROMPT = "cal> ";
-    private final static String [] WEEKDAY = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
-    public int parseDay(String weekday) {
-        return Arrays.asList(WEEKDAY).indexOf(weekday);
-    }
 
     public void runPrompt() {
         Calendar calendar = new Calendar();
@@ -17,7 +13,6 @@ public class Prompt {
 
         int month;
         int year;
-        int weekday;
 
         while(true) {
             System.out.println("년도을 입력하세요");
@@ -27,26 +22,14 @@ public class Prompt {
             System.out.println("달을 입력하세요");
             System.out.print("Month> ");
             month = scanner.nextInt();
-
-            System.out.println("첫 째날의 요일을 입력하세요(SU, MO, TU, WE, TH, FR, SA)");
-            System.out.print("Weekday> ");
-            String inputStr = scanner.next();
-            if (!(Arrays.stream(WEEKDAY).anyMatch(inputStr::equals))) {
-                System.out.println("SU, MO, TU, WE, TH, FR, SA 중 하나를 입력해주세요.");
-                System.out.print("Weekday> ");
-                inputStr = scanner.next();
-            }
-
-            weekday = parseDay(inputStr);
-
-            if (month == -1) {
+            if (year < 1970 || month < 1) {
                 break;
             }
             if (month > 12) {
                 continue;
             }
 
-            calendar.printCalendar(year, month, weekday);
+            calendar.printCalendar(year, month);
 
         }
 
