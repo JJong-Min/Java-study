0fa8390eccf1e409c636cc4c5f5b4800db2560b8
JJong-Min <sambujakkk@naver.com>
2021-08-01 17:01:12 +0900

    Update Prompt class
    사용자에게 해당 월의 첫 번째 요일을 입력받아 값을 넘기도록 method를 수정했습니다.

--- JavaPlayInGround/calendar/src/calendar/Prompt.java
+++ JavaPlayInGround/calendar/src/calendar/Prompt.java
@@ -1,10 +1,15 @@
 package calendar;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Prompt {
 
-    private  final static String PROMPT = "cal> ";
+    private final static String PROMPT = "cal> ";
+    private final static String [] WEEKDAY = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
+    public int parseDay(String weekday) {
+        return Arrays.asList(WEEKDAY).indexOf(weekday);
+    }
 
     public void runPrompt() {
         Calendar calendar = new Calendar();
@@ -12,14 +17,28 @@ public class Prompt {
 
         int month;
         int year;
+        int weekday;
 
         while(true) {
             System.out.println("년도을 입력하세요");
             System.out.print("Year> ");
             year = scanner.nextInt();
+
             System.out.println("달을 입력하세요");
             System.out.print("Month> ");
             month = scanner.nextInt();
+
+            System.out.println("첫 째날의 요일을 입력하세요(SU, MO, TU, WE, TH, FR, SA)");
+            System.out.print("Weekday> ");
+            String inputStr = scanner.next();
+            if (!(Arrays.stream(WEEKDAY).anyMatch(inputStr::equals))) {
+                System.out.println("SU, MO, TU, WE, TH, FR, SA 중 하나를 입력해주세요.");
+                System.out.print("Weekday> ");
+                inputStr = scanner.next();
+            }
+
+            weekday = parseDay(inputStr);
+
             if (month == -1) {
                 break;
             }
@@ -27,7 +46,7 @@ public class Prompt {
                 continue;
             }
 
-            calendar.printCalendar(year, month);
+            calendar.printCalendar(year, month, weekday);
 
         }
 
