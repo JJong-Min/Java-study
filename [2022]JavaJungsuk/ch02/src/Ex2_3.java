2e6a9402f408dfde17054146b30db0f201f5eed8
JJong-Min <sambujakkk@naver.com>
2022-01-22 00:36:00 +0900

    변수와 상수

---
+++ [2022]JavaJungsuk/ch02/src/Ex2_3.java
@@ -0,0 +1,11 @@
+public class Ex2_3 {
+    public static void main(String[] args) {
+        int score = 100;
+        final int constScore = 300; //final은 상수(constant)를 의미
+        System.out.println(score);
+        System.out.println(constScore);
+
+        score = 200;
+        System.out.println(score);
+    }
+}
