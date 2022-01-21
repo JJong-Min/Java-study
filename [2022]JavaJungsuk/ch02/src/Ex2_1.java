131367e7b589f95803ddc56c5f39cfd4f41ba6b9
JJong-Min <sambujakkk@naver.com>
2022-01-21 23:51:49 +0900

    사칙연산

---
+++ [2022]JavaJungsuk/ch02/src/Ex2_1.java
@@ -0,0 +1,11 @@
+public class Ex2_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello"); // system.out.print는 줄바꿈을 안함.
+
+        System.out.println(3+5);
+        System.out.println(3-5);
+        System.out.println(3*5);
+        System.out.println(5/3); // 결과: 1
+        System.out.println(5/3.0); // 결과: 1.66666666667
+    }
+}
