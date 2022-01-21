0abf854c4393fa471eb50ab1df4de90aa04f4b4b
JJong-Min <sambujakkk@naver.com>
2022-01-22 01:48:52 +0900

    리터럴(literal)

--- [2022]JavaJungsuk/ch02/src/Ex2_3.java
+++ [2022]JavaJungsuk/ch02/src/Ex2_3.java
@@ -7,5 +7,11 @@ public class Ex2_3 {
 
         score = 200;
         System.out.println(score);
+
+        int oct = 010; // 8진수, 10진수로는 8
+        int hex = 0x10; // 16진수, 10진수로는 16
+        System.out.println(oct); // println은 10진수로출력되고 접두사, 접미사는 생략
+        System.out.println(3.14f);
+        System.out.println(1e3);
     }
 }
