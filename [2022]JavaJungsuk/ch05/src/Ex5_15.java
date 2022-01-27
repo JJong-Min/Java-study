31e995423f0bd53f190dcab912f656409fd4b6b9
JJong-Min <sambujakkk@naver.com>
2022-01-28 03:05:05 +0900

    string클래스

---
+++ [2022]JavaJungsuk/ch05/src/Ex5_15.java
@@ -0,0 +1,9 @@
+public class Ex5_15 {
+    public static void main(String[] args) {
+        String str = "ABCDE";
+        char ch = str.charAt(0);
+        String str2 = str.substring(1, 4);
+        System.out.println(ch);
+        System.out.println(str2);
+    }
+}
