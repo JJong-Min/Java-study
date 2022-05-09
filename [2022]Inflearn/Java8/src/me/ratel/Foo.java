50c996809494cea8448648ecdf3a0bbf283001d1
JJong-Min <sambujakkk@naver.com>
2022-05-09 20:02:32 +0900

    add code

--- [2022]Inflearn/Java8/src/me/ratel/Foo.java
+++ [2022]Inflearn/Java8/src/me/ratel/Foo.java
@@ -1,9 +1,17 @@
 package me.ratel;
 
+import java.util.function.Function;
+
 public class Foo {
 
   public static void main(String[] args) {
     RunsomethingOne runsomethingOne = () -> System.out.println("ratel");
     RunSomethingTwo runSomethingTwo = number -> number + 1;
+
+    Plus10 plus10 = new Plus10();
+    System.out.println(plus10.apply(10));
+
+    Function<Integer, Integer> multiply10 = (number) -> number * 10;
+    System.out.println(multiply10.apply(10));
   }
 }
--- [2022]Inflearn/Java8/src/me/ratel/RunSomethingTwo.java
+++ [2022]Inflearn/Java8/src/me/ratel/RunSomethingTwo.java
@@ -2,5 +2,7 @@ package me.ratel;
 
 @FunctionalInterface
 public interface RunSomethingTwo {
+  // 이러한 functional interface는 사실 상 필요없음.
+  // 이미 java단에서 Function이라는 interface를 제공 중,
   int doIt(int number);
 }
