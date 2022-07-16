613ecb9a2bdba37d4f9b6b3b017d93e60139b3fa
JJong-Min <sambujakkk@naver.com>
2022-07-16 15:19:14 +0900

    implement CurrentConditionsDisplay class

--- [2022]HeadFirstDP/src/main/java/Section2/observer/CurrentConditionsDisplay.java
+++ [2022]HeadFirstDP/src/main/java/Section2/observer/CurrentConditionsDisplay.java
@@ -1,5 +1,29 @@
 package Section2.observer;
 
-public class CurrentConditionsDisplay {
+import Section2.display.DisplayElement;
+import Section2.subject.WeatherData;
+import java.net.http.WebSocketHandshakeException;
 
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+  private float temperature;
+  private float humidity;
+  private WeatherData weatherData;
+
+  public CurrentConditionsDisplay(WeatherData weatherData) {
+    this.weatherData = weatherData;
+    weatherData.registerObserver(this);
+  }
+
+  @Override
+  public void update(float temp, float humidity, float pressure) {
+    this.temperature = temp;
+    this.humidity = humidity;
+    display();
+  }
+
+  @Override
+  public void display() {
+    System.out.println("현재 상태: 온도 " + temperature + "F, 습도 " +  humidity + "%");
+  }
 }
