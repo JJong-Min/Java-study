f07627d60f34f15a6d73957d19d6c5f17a67d621
JJong-Min <sambujakkk@naver.com>
2022-07-16 14:21:12 +0900

    add base code

---
+++ [2022]HeadFirstDP/src/main/java/Section2/WeatherData.java
@@ -0,0 +1,20 @@
+package Section2;
+
+public class WeatherData {
+
+  public void getTemperature() {
+
+  }
+
+  public void getPressure() {
+
+  }
+
+  public void getHumidity() {
+
+  }
+
+  public void measurementsChanged() {
+
+  }
+}
